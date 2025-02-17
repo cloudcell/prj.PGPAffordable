c49262a9f8d3400c33f2a84da9024a8028e9cd45
cloudcell <cloudcell@users.noreply.github.com>
2025-02-18 04:29:07 +1300

    adds script descriptions

--- ot_activity_class_getter.py
+++ ot_activity_class_getter.py
@@ -1,39 +1,43 @@
-import requests
+"""
+This script is used to get the unique classes of activities from the generated intermediary database.
+"""
 
-# Base URL for the Open Targets API
-base_url = "https://api.platform.opentargets.org/api/v4"
+# import requests
 
-# Endpoint for the GraphQL API
-graphql_endpoint = f"{base_url}/graphql"
+# # Base URL for the Open Targets API
+# base_url = "https://api.platform.opentargets.org/api/v4"
 
-# Query to inspect the schema
-query = {
-    "query": """
-    {
-      __schema {
-        types {
-          name
-          fields {
-            name
-          }
-        }
-      }
-    }
-    """
-}
+# # Endpoint for the GraphQL API
+# graphql_endpoint = f"{base_url}/graphql"
 
-# Send a POST request to the API
-response = requests.post(graphql_endpoint, json=query)
+# # Query to inspect the schema
+# query = {
+#     "query": """
+#     {
+#       __schema {
+#         types {
+#           name
+#           fields {
+#             name
+#           }
+#         }
+#       }
+#     }
+#     """
+# }
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
-    schema = data.get("data", {}).get("__schema", {}).get("types", [])
-    for type_ in schema:
-        print(f"Type: {type_['name']}")
-        if type_["fields"]:
-            print("  Fields:")
-            for field in type_["fields"]:
-                print(f"    {field['name']}")
-else:
-    print(f"Failed to query schema. Status code: {response.status_code}, Error: {response.text}")
+# # Send a POST request to the API
+# response = requests.post(graphql_endpoint, json=query)
+
+# # Check if the request was successful
+# if response.status_code == 200:
+#     data = response.json()
+#     schema = data.get("data", {}).get("__schema", {}).get("types", [])
+#     for type_ in schema:
+#         print(f"Type: {type_['name']}")
+#         if type_["fields"]:
+#             print("  Fields:")
+#             for field in type_["fields"]:
+#                 print(f"    {field['name']}")
+# else:
+#     print(f"Failed to query schema. Status code: {response.status_code}, Error: {response.text}")
