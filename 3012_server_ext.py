20cf49690a582f19040df5932da12fee7b036554
cloudcell <cloudcell@users.noreply.github.com>
2025-02-22 08:50:07 +1300

    amends server port number

--- 3012_server_ext.py
+++ 3012_server_ext.py
@@ -107,4 +107,4 @@ def search_targets(query: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7334)
