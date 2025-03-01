2b0ed174e7d41151ce91a368b23131bafdc0dc88
Yakovenko Igor <yakovenko.igor.97@gmail.com>
2025-03-01 13:57:42 +0300

    split script 0120 into 0120 and 0121

--- 0120_dbase_json_to_sparse_vectors_tsv.py
+++ 0120_dbase_json_to_sparse_vectors_tsv.py
@@ -25,6 +25,8 @@ target_ids = [row[0] for row in con.execute(targets_query).fetchall()]
 vectors_query = "SELECT ChEMBL_id, vector FROM tbl_molecular_vectors"
 vector_data = con.execute(vectors_query).fetchall()
 
+con.close()
+
 # Initialize a DataFrame with ChEMBL IDs as rows and target IDs as columns
 vector_array = pd.DataFrame(index=[row[0] for row in vector_data], columns=target_ids, dtype=np.float32).fillna(0.0)
 
@@ -38,17 +40,6 @@ for chembl_id, vector_json in tqdm(vector_data, desc="Processing molecular vecto
 del vector_data
 gc.collect()
 
-con.execute("DROP TABLE IF EXISTS tbl_vector_array")
-
-# Create a new table for the vector array
-column_definitions = ", ".join([f'"{col}" FLOAT' for col in target_ids])
-create_table_query = f"""
-    CREATE TABLE IF NOT EXISTS tbl_vector_array (
-        ChEMBL_id STRING PRIMARY KEY, {column_definitions}
-    )
-"""
-con.execute(create_table_query)
-
 # Insert data into DuckDB with progress bar
 data_tuples = [tuple([chembl_id] + row.tolist()) for chembl_id, row in vector_array.iterrows()]
 
@@ -57,24 +48,11 @@ header = ['ChEMBL_id'] + list(vector_array.columns)
 del vector_array
 gc.collect()
 
-for i1 in tqdm(range(int(len(data_tuples) / BATCH_SIZE))):
-    i1 *= BATCH_SIZE
-    i2 = i1 + BATCH_SIZE
-    with open(TEMP_TSV_PATH, 'w', encoding='utf-8') as f:
-        f.write('\t'.join(map(str, header)) + '\n')
+with open(TEMP_TSV_PATH, 'w', encoding='utf-8') as f:
+    f.write('\t'.join(map(str, header)) + '\n')
+    for i1 in tqdm(range(int(len(data_tuples) / BATCH_SIZE))):
+        i1 *= BATCH_SIZE
+        i2 = i1 + BATCH_SIZE
         f.write('\n'.join('\t'.join(map(str, row)) for row in data_tuples[i1:i2]))
 
-    con.execute(f"""
-        COPY tbl_vector_array FROM '{TEMP_TSV_PATH}'
-        (FORMAT CSV, HEADER TRUE, DELIMITER '\t', QUOTE '', ESCAPE '', NULL '{NULL}', AUTO_DETECT FALSE)
-    """)
-
-# Verify insertion
-con.sql("SELECT * FROM tbl_vector_array LIMIT 10").show()
-print(f'{con.execute("SELECT count(*) FROM tbl_vector_array").fetchone()[0]} rows')
-print(f'{len(con.execute("SELECT * FROM tbl_vector_array LIMIT 1").fetchone())} columns')
-
-# Close connection
-con.close()
-
-print("✅ Vector array creation completed and stored in DuckDB.")
+print(f"✅ Vector array table saved in {TEMP_TSV_PATH}.")
---
+++ 0121_tsv_sparse_vectors_injest.py
@@ -0,0 +1,58 @@
+"""
+This script is used to create a matrix with rows-vectors from the generated vectorized molecular profiles in json format.
+"""
+import duckdb
+from tqdm import tqdm
+
+
+TEMP_TSV_PATH = "data_tmp/temp_data.tsv"
+TEMP_TSV_PATH_2 = "data_tmp/temp_data_2.tsv"
+BATCH_SIZE = 10 # rows
+NULL = '<NULL>'
+
+# Connect to DuckDB database
+db_path = "bio_data.duck.db"
+con = duckdb.connect(db_path)
+
+total = con.execute("SELECT count(*) FROM tbl_molecular_vectors").fetchone()[0]
+
+def save_batch_to_db(con: duckdb.DuckDBPyConnection, batch: list[str]):
+    with open(TEMP_TSV_PATH_2, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(batch))
+    con.execute(f"""
+        COPY tbl_vector_array FROM '{TEMP_TSV_PATH}'
+        (FORMAT CSV, HEADER TRUE, DELIMITER '\t', QUOTE '', ESCAPE '', NULL '{NULL}', AUTO_DETECT FALSE)
+    """)
+
+con.execute("DROP TABLE IF EXISTS tbl_vector_array")
+
+with open(TEMP_TSV_PATH, "r", encoding='utf-8') as f:
+    header = next(f)
+
+    # Create a new table for the vector array
+    column_definitions = ", ".join([f'"{col}" FLOAT' for col in header[1:]])
+    create_table_query = f"""
+        CREATE TABLE IF NOT EXISTS tbl_vector_array (
+            ChEMBL_id STRING PRIMARY KEY, {column_definitions}
+        )
+    """
+    con.execute(create_table_query)
+
+    batch = [header]
+    for line in tqdm(f, total=total):
+        batch.append(line)
+        if len(batch) == BATCH_SIZE:
+            save_batch_to_db(con, batch)
+            batch = [header]
+    if len(batch) > 1:
+        save_batch_to_db(con, batch)
+
+# Verify insertion
+con.sql("SELECT * FROM tbl_vector_array LIMIT 10").show()
+print(f'{con.execute("SELECT count(*) FROM tbl_vector_array").fetchone()[0]} rows')
+print(f'{len(con.execute("SELECT * FROM tbl_vector_array LIMIT 1").fetchone())} columns')
+
+# Close connection
+con.close()
+
+print("✅ Vector array creation completed and stored in DuckDB.")
