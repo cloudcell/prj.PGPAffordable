f6ffd69c482f38cf94d614ffa8b73bc484242dce
Yakovenko Igor <yakovenko.igor.97@gmail.com>
2025-03-04 23:09:06 +0300

    updates ftp data getter

--- lib_utils/ftp_json_data_getter.py
+++ lib_utils/ftp_json_data_getter.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from ftplib import FTP, error_temp, error_perm, error_proto, error_reply
 import os
 import socket
@@ -16,9 +17,34 @@ def _get_file_size(ftp, filename):
         return None
 
 
+def _get_hash_of_file(path_to_file):
+    with open(path_to_file, 'rb') as f:
+        file_hash = md5(f.read()).hexdigest()
+    return file_hash
+
+
+def _save_hash_of_file(path_to_file, path_to_hash):
+    file_hash = _get_hash_of_file(path_to_file)
+    with open(path_to_hash, 'w') as f:
+        f.write(file_hash)
+
+
 def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_limit, progress_bar, progress_lock):
     """Downloads a file, resuming if it's partially downloaded, and updates the tqdm progress bar."""
     local_path = os.path.join(local_dir, filename)
+    path_to_hash = local_path + '.md5'
+
+    if os.path.exists(path_to_hash):
+        file_hash = _get_hash_of_file(local_path)
+
+        with open(path_to_hash) as f:
+            saved_hash = f.read()
+        
+        if file_hash == saved_hash:
+            print(f"Skipping (already complete): {filename}")
+            with progress_lock:
+                progress_bar.update(1)
+            return
 
     retries = 0
     while retries < retry_limit:
@@ -38,6 +64,7 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
                     local_size = os.path.getsize(local_path)
 
                     if local_size == remote_size:
+                        _save_hash_of_file(local_path, path_to_hash)
                         print(f"Skipping (already complete): {filename}")
                         with progress_lock:
                             progress_bar.update(1)
@@ -58,6 +85,7 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
 
                     ftp.retrbinary(f"RETR {filename}", f.write, rest=local_size)
 
+                _save_hash_of_file(local_path, path_to_hash)
                 print(f"Downloaded: {filename}")
                 with progress_lock:
                     progress_bar.update(1)
@@ -76,7 +104,7 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
         progress_bar.update(1)  # Mark as "processed" to avoid blocking progress
 
 
-def download_json_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 3, n_threads = 4):
+def download_json_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 8, n_threads = 4):
     """Lists all JSON files and downloads them using multiple threads with resume support and progress bar."""
     os.makedirs(local_dir, exist_ok=True)
 
