cabe8a2199017e0057a0bc3c3c5bc8a99e7022c7
Yakovenko Igor <yakovenko.igor.97@gmail.com>
2025-03-05 03:15:10 +0300

    updates 0090

--- 0090_dbase_molecule_associated_disease_injest.py
+++ 0090_dbase_molecule_associated_disease_injest.py
@@ -7,30 +7,30 @@ from tqdm import tqdm
 
 
 
-data_dir = "data_tmp"
-
+DATA_DIR = "data/202409XX/molecule"
 db_path = "bio_data.duck.db"
+
 con = duckdb.connect(db_path)
 
-files = [filename for filename in os.listdir(data_dir) if filename.startswith("mol_disease_") and filename.endswith(".json")]
+files = [filename for filename in os.listdir(DATA_DIR) if filename.endswith(".json")]
+records = []
 
-for filename in tqdm(files):
-    file_path = os.path.join(data_dir, filename)
-    
+for filename in files:
+    file_path = os.path.join(DATA_DIR, filename)
     with open(file_path, "r", encoding="utf-8") as f:
-        associated_diseases_list = json.load(f)
+        for line in f:
+            line = line.replace('\t', ' ')
+            record = json.loads(line)
+            records.append(record)
 
-    # Extract ChEMBL ID
-    chembl_id = filename.replace("mol_disease_", "").replace(".json", "")
 
-    for row in associated_diseases_list:
-        disease_id = row['id']
+q = 'INSERT OR IGNORE INTO tbl_disease_substance VALUES ($disease_id, $chembl_id)'
+for record in tqdm(records):
+    chembl_id = record['id']
+    for disease_id in record.get('linkedDiseases', {}).get('rows', []):
+        con.execute(q, {'disease_id': disease_id, 'chembl_id': chembl_id})
 
-        q = 'INSERT OR IGNORE INTO tbl_disease_substance VALUES ($disease_id, $chembl_id)'
-        params = {'disease_id': disease_id, 'chembl_id': chembl_id}
-        con.execute(q, params)
 
-print(f'tbl_diseases: {con.execute("SELECT count(*) FROM tbl_diseases").fetchone()[0]} rows')
 print(f'tbl_disease_substance: {con.execute("SELECT count(*) FROM tbl_disease_substance").fetchone()[0]} rows')
 
 con.close()
