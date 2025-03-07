c6f2f343f38493e8d5a3152488e01b9b720c1ff1
Alex Bikeyev <alex.bikeyev@protonmail.com>
2025-03-08 05:44:42 +1300

    increases the number of retries to 10

--- lib_utils/ftp_parquet_data_getter.py
+++ lib_utils/ftp_parquet_data_getter.py
@@ -93,7 +93,7 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, remote_size, ftp_time
     _has_errors = True
 
 
-def download_parquet_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 8, n_threads = 4):
+def download_parquet_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 10, n_threads = 4):
     """dDownload all .parquet files recursively using multiple threads with resume support and progress bar."""
     os.makedirs(local_dir, exist_ok=True)
 
