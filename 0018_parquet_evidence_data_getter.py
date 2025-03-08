a79179976a74d3e3872ec6219cf601ea33f81fb1
Yakovenko Igor <yakovenko.igor.97@gmail.com>
2025-03-08 15:58:30 +0300

    updates parquet data getter: check sha1 hash

--- 0018_parquet_evidence_data_getter.py
+++ 0018_parquet_evidence_data_getter.py
@@ -4,5 +4,6 @@ from lib_utils.ftp_parquet_data_getter import download_parquet_files
 FTP_HOST = "ftp.ebi.ac.uk"
 FTP_DIR = "/pub/databases/opentargets/platform/24.09/output/etl/parquet/evidence/"
 LOCAL_DIR = "data/202409XX/evidence"  # Change this to your desired local directory
+CHECKSUM_FILE = 'data/202409XX/release_data_integrity'
 
-download_parquet_files(FTP_HOST, FTP_DIR, LOCAL_DIR, n_threads=1)
+download_parquet_files(FTP_HOST, FTP_DIR, LOCAL_DIR, CHECKSUM_FILE)
--- lib_utils/ftp_parquet_data_getter.py
+++ lib_utils/ftp_parquet_data_getter.py
@@ -17,35 +17,13 @@ def _get_hash_of_file(path_to_file):
     return file_hash
 
 
-def _save_hash_of_file(path_to_file, path_to_hash):
-    file_hash = _get_hash_of_file(path_to_file)
-    with open(path_to_hash, 'w') as f:
-        f.write(file_hash)
-
-
-def _download_file(ftp_host, ftp_dir, local_dir, filename, remote_size, ftp_timeout, retry_limit, progress_bar, progress_lock):
+def _download_file(ftp_host, ftp_dir, local_dir, filename, saved_hash, remote_size, ftp_timeout, retry_limit, progress_bar, progress_lock):
     """Downloads a file, resuming if it's partially downloaded, and updates the tqdm progress bar."""
     os.makedirs(local_dir, exist_ok=True)
     local_path = os.path.join(local_dir, filename)
-    # path_to_hash = local_path + '.md5'
-    path_to_hash = local_path + '.sha1'
-
     if os.path.exists(local_path):
-        if os.path.exists(path_to_hash):
-            file_hash = _get_hash_of_file(local_path)
-
-            with open(path_to_hash) as f:
-                saved_hash = f.read()
-            
-            if file_hash == saved_hash:
-                print(f"Skipping (already complete): {filename}")
-                with progress_lock:
-                    progress_bar.update(1)
-                return
-
-        local_size = os.path.getsize(local_path)
-        if local_size == remote_size:
-            _save_hash_of_file(local_path, path_to_hash)
+        file_hash = _get_hash_of_file(local_path)
+        if file_hash == saved_hash:
             print(f"Skipping (already complete): {filename}")
             with progress_lock:
                 progress_bar.update(1)
@@ -76,7 +54,6 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, remote_size, ftp_time
 
                     ftp.retrbinary(f"RETR {filename}", f.write, rest=local_size)
 
-                _save_hash_of_file(local_path, path_to_hash)
                 print(f"Downloaded: {os.path.join(ftp_dir, filename)}")
                 with progress_lock:
                     progress_bar.update(1)
@@ -95,9 +72,20 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, remote_size, ftp_time
     _has_errors = True
 
 
-def download_parquet_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 10, n_threads = 4):
+def download_parquet_files(ftp_host: str, ftp_dir: str, local_dir: str, checksum_file: str, ftp_timeout = 30, retry_limit = 10, n_threads = 4):
     """dDownload all .parquet files recursively using multiple threads with resume support and progress bar."""
     os.makedirs(local_dir, exist_ok=True)
+
+    if not os.path.exists(checksum_file):
+        print(f"error: {checksum_file} not exists")
+        exit(1)
+
+    with open(checksum_file) as f:
+        checksum_dict = dict(reversed(line.split(maxsplit=1)) for line in f.read().split('\n') if './output/etl/' in line)
+
+    # filter by ftp_dir to optimize dict size
+    rel_path = ftp_dir.split('/output/etl/', maxsplit=1)[-1]
+    checksum_dict = {k.replace('./output/etl/', ''): v  for k, v in checksum_dict.items() if rel_path in k}
 
     def get_files_recursively(ftp: FTP, dir: str):
         print(dir)
@@ -135,7 +123,9 @@ def download_parquet_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_time
         # Multi-threaded download
         threads = []
         for i, (dir, filename, size) in enumerate(parquet_files):
-            thread = Thread(daemon=True, target=_download_file, args=(ftp_host, dir, os.path.join(local_dir, dir.replace(ftp_dir, '', 1)), filename, size, ftp_timeout, retry_limit, progress_bar, progress_lock))
+            saved_hash = checksum_dict[os.path.join(dir, filename).split('/output/etl/', maxsplit=1)[-1]]
+
+            thread = Thread(daemon=True, target=_download_file, args=(ftp_host, dir, os.path.join(local_dir, dir.replace(ftp_dir, '', 1)), filename, saved_hash, size, ftp_timeout, retry_limit, progress_bar, progress_lock))
             threads.append(thread)
             thread.start()
 
