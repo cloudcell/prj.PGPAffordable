a2605047972fe20aea28f4b715ee1e77baa3f094
Yakovenko Igor <yakovenko.igor.97@gmail.com>
2025-03-08 15:51:12 +0300

    updates json data getter: check sha1 hash

--- 0017_json_targets_data_getter.py
+++ 0017_json_targets_data_getter.py
@@ -4,5 +4,6 @@ from lib_utils.ftp_json_data_getter import download_json_files
 FTP_HOST = "ftp.ebi.ac.uk"
 FTP_DIR = "/pub/databases/opentargets/platform/24.09/output/etl/json/targets/"
 LOCAL_DIR = "data/202409XX/targets"  # Change this to your desired local directory
+CHECKSUM_FILE = 'data/202409XX/release_data_integrity'
 
-download_json_files(FTP_HOST, FTP_DIR, LOCAL_DIR, n_threads=1)
+download_json_files(FTP_HOST, FTP_DIR, LOCAL_DIR, CHECKSUM_FILE, n_threads=1)
--- lib_utils/ftp_json_data_getter.py
+++ lib_utils/ftp_json_data_getter.py
@@ -26,24 +26,11 @@ def _get_hash_of_file(path_to_file):
     return file_hash
 
 
-def _save_hash_of_file(path_to_file, path_to_hash):
-    file_hash = _get_hash_of_file(path_to_file)
-    with open(path_to_hash, 'w') as f:
-        f.write(file_hash)
-
-
-def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_limit, progress_bar, progress_lock):
+def _download_file(ftp_host, ftp_dir, local_dir, filename, saved_hash, ftp_timeout, retry_limit, progress_bar, progress_lock):
     """Downloads a file, resuming if it's partially downloaded, and updates the tqdm progress bar."""
     local_path = os.path.join(local_dir, filename)
-    # path_to_hash = local_path + '.md5'
-    path_to_hash = local_path + '.sha1'
-
-    if os.path.exists(local_path) and os.path.exists(path_to_hash):
+    if os.path.exists(local_path):
         file_hash = _get_hash_of_file(local_path)
-
-        with open(path_to_hash) as f:
-            saved_hash = f.read()
-        
         if file_hash == saved_hash:
             print(f"Skipping (already complete): {filename}")
             with progress_lock:
@@ -68,7 +55,6 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
                     local_size = os.path.getsize(local_path)
 
                     if local_size == remote_size:
-                        _save_hash_of_file(local_path, path_to_hash)
                         print(f"Skipping (already complete): {filename}")
                         with progress_lock:
                             progress_bar.update(1)
@@ -89,7 +75,6 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
 
                     ftp.retrbinary(f"RETR {filename}", f.write, rest=local_size)
 
-                _save_hash_of_file(local_path, path_to_hash)
                 print(f"Downloaded: {filename}")
                 with progress_lock:
                     progress_bar.update(1)
@@ -109,9 +94,20 @@ def _download_file(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_li
     _has_errors = True
 
 
-def download_json_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout = 30, retry_limit = 10, n_threads = 4):
+def download_json_files(ftp_host: str, ftp_dir: str, local_dir: str, checksum_file: str, ftp_timeout = 30, retry_limit = 10, n_threads = 4):
     """Lists all JSON files and downloads them using multiple threads with resume support and progress bar."""
     os.makedirs(local_dir, exist_ok=True)
+
+    if not os.path.exists(checksum_file):
+        print(f"error: {checksum_file} not exists")
+        exit(1)
+
+    with open(checksum_file) as f:
+        checksum_dict = dict(reversed(line.split(maxsplit=1)) for line in f.read().split('\n') if './output/etl/' in line)
+
+    # filter by ftp_dir to optimize dict size
+    rel_path = ftp_dir.split('/output/etl/', maxsplit=1)[-1]
+    checksum_dict = {k.replace('./output/etl/', ''): v  for k, v in checksum_dict.items() if rel_path in k}
 
     try:
         with FTP(ftp_host, timeout=ftp_timeout) as ftp:
@@ -133,7 +129,9 @@ def download_json_files(ftp_host: str, ftp_dir: str, local_dir: str, ftp_timeout
         # Multi-threaded download
         threads = []
         for i, filename in enumerate(json_files):
-            thread = Thread(daemon=True, target=_download_file, args=(ftp_host, ftp_dir, local_dir, filename, ftp_timeout, retry_limit, progress_bar, progress_lock))
+            saved_hash = checksum_dict[os.path.join(ftp_dir, filename).split('/output/etl/', maxsplit=1)[-1]]
+
+            thread = Thread(daemon=True, target=_download_file, args=(ftp_host, ftp_dir, local_dir, filename, saved_hash, ftp_timeout, retry_limit, progress_bar, progress_lock))
             threads.append(thread)
             thread.start()
 
